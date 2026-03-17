581330ea77ed0601c7df6406d5a054082e22e7a3
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-03-17 11:09:58 +0300

    testEmptyVector

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
 #include "top_ip_vector.hpp"
+bool testEmptyVector()
+{
+  return false;
+}
 int main()
-{}
+{
+  using test_t = std::pair < const char*, bool(*)() >;
+  test_t tests[] =
+  {
+    {
+      "Empty vector", testEmptyVector
+    }
+  };
+  /*
+  bool(*tests[])() = {
+    testEmptyVector
+  }; */
+  const size_t count = sizeof(tests) / sizeof(bool(*)());
+  for (size_t i = 0; i < count; ++i)
+  {
+    std::cout << testEmptyVector() << "\n";
+  }
+}
